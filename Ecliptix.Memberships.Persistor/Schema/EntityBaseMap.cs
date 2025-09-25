d78352409d1fca7460b436d08bbab0b15a48f742
MrReptiloid <morgot1999@gmail.com>
2025-09-25 10:39:42 +0300

    feat: Enhance EntityBaseMap with unique and filtered indexes for CreatedAt and UpdatedAt properties

--- Ecliptix.Memberships.Persistor/Schema/EntityBaseMap.cs
+++ Ecliptix.Memberships.Persistor/Schema/EntityBaseMap.cs
@@ -23,6 +23,18 @@ public abstract class EntityBaseMap<T> : EntityTypeConfiguration<T> where T : En
         entity.Property(e => e.IsDeleted)
             .HasDefaultValue(false);
         
-        entity.HasIndex(e => e.UniqueId).IsUnique();
+        entity.HasIndex(e => e.UniqueId)
+            .IsUnique()
+            .HasDatabaseName($"UQ_{typeof(T).Name}_UniqueId");
+
+        entity.HasIndex(e => e.CreatedAt)
+            .IsDescending()
+            .HasFilter("IsDeleted = 0")
+            .HasDatabaseName($"IX_{typeof(T).Name}_CreatedAt");
+        
+        entity.HasIndex(e => e.UpdatedAt)
+            .IsDescending()
+            .HasFilter("IsDeleted = 0")
+            .HasDatabaseName($"IX_{typeof(T).Name}_UpdatedAt");
     }
 }
